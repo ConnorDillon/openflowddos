4bd19cc5bc133869f597e4ad16709a49046c20fa
Connor Dillon <cdillon@os3.nl>
2014-02-09 19:23:48 +0100

    ryu controller v1.0

--- ryu_manager.py
+++ ryu_manager.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 #
-# Copyright (C) 2011, 2012 Nippon Telegraph and Telephone Corporation.
-# Copyright (C) 2011 Isaku Yamahata <yamahata at valinux co jp>
+# Copyright 2014 C. Dillon
+# Copyright 2014 M. Berkelaar
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -11,26 +11,15 @@
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
-# implied.
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
+#
 from ryu.lib import hub
 hub.patch(thread=False)
-
-# TODO:
-#   Right now, we have our own patched copy of ovs python bindings
-#   Once our modification is upstreamed and widely deployed,
-#   use it
-#
-# NOTE: this modifies sys.path and thus affects the following imports.
-# eg. oslo.config.cfg.
-
 import logging
 from ryu import log
 log.early_init_log(logging.DEBUG)
-
 from ryu.app import wsgi
 from ryu.base.app_manager import AppManager
 
